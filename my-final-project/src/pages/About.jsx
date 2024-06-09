1562b58dffa7914ccdc470204d4a567eda1bfc03
Farah Sallam <farahsallam@student.olympic.edu>
2024-06-09 16:22:27 -0700

    added to the about page

--- my-final-project/src/pages/About.jsx
+++ my-final-project/src/pages/About.jsx
@@ -8,7 +8,10 @@ const About = () => {
           <div className="wrapper">
             <div className="title">About Me</div>
             <div className="content">
-              
+              <p>My name is Farah and I'm a twenty-something Library Tech interested in birds, knitted crafting, programming, and literature.</p>
+              <p>I was born and raised in Northern California, but about a year ago, I took flight, and headed towards the Pacific Northwest which I now reside.</p>
+              <p>This site is my final project for a web scripting class. However, this site was also created as a way to display my newfound love for birds.</p>
+              <p>I've only seriously started birding for about a month, but I gained much interest thanks to my partner and his interest. </p>
             </div>
           </div>
         </main>
